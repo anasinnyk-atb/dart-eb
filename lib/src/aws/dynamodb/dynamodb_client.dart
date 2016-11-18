9e95cdfca0cffc7f7838c8ff45eec89f7ebac068
Andrii Nasinnyk <anasinnyk@atypicalbrands.com>
2016-11-18 14:35:36 +0200

    aws-sdk

--- lib/src/aws/dynamodb/dynamodb_client.dart
+++ lib/src/aws/dynamodb/dynamodb_client.dart
@@ -9,12 +9,3 @@ class DynamoDbClient extends Client {
     return this.call('GetItem', data);
   }
 }
-
-void main() {
-  new DynamoDbClient(new Credentials())..getItem({
-    'TableName': 'dev-atypical-checkout',
-    'Key': {
-      'uuid': {'S': 'fa3645ed-2b2e-4c56-bdcf-5b19cafaa777'},
-    }
-  }).then((response) => print('${response.statusCode}\n${response.body}'));
-}
