4854d317ea0662ffe9e78dd42d9c94581a52446a
Andrii Nasinnyk <anasinnyk@atypicalbrands.com>
2016-11-21 21:30:15 +0200

    add methods

--- lib/src/aws/dynamodb/dynamodb_client.dart
+++ lib/src/aws/dynamodb/dynamodb_client.dart
@@ -5,7 +5,18 @@ class DynamoDbClient extends Client {
 
   DynamoDbClient(credentials): super(credentials);
 
-  getItem(dynamic data) {
-    return this.call('GetItem', data);
-  }
+  batchGetItem(dynamic data) => this.call('BatchGetItem', data);
+  batchWriteItem(dynamic data) => this.call('BatchWriteItem', data);
+  createTable(dynamic data) => this.call('CreateTable', data);
+  deleteItem(dynamic data) => this.call('DeleteItem', data);
+  deleteTable(dynamic data) => this.call('DeleteTable', data);
+  describeLimits(dynamic data) => this.call('DescribeLimits', data);
+  describeTable(dynamic data) => this.call('DescribeTable', data);
+  getItem(dynamic data) => this.call('GetItem', data);
+  listTables(dynamic data) => this.call('ListTables', data);
+  putItem(dynamic data) => this.call('PutItem', data);
+  query(dynamic data) => this.call('Query', data);
+  scan(dynamic data) => this.call('Scan', data);
+  updateItem(dynamic data) => this.call('UpdateItem', data);
+  updateTable(dynamic data) => this.call('UpdateTable', data);
 }
